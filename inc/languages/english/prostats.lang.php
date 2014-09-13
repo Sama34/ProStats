cbdd8885921993e1ed1b872638023f3761cf233c
Omar Gonzalez <bitbucket@omarg.me>
2014-09-13 14:24:53 -0700

    ProStats 1.2

---
+++ inc/languages/english/prostats.lang.php
@@ -0,0 +1,30 @@
+<?php
+$l['prostats_prostats'] = "Forum stats";
+
+$l['prostats_username'] = "Username";
+$l['prostats_posts'] = "Posts";
+$l['prostats_datetime'] = "Date, time";
+$l['prostats_forum'] = "Forum";
+
+$l['prostats_top_posters'] = "Top posters";
+$l['prostats_top_topreferrers'] = "Top referrers";
+$l['prostats_most_thanks'] = "Most thanks";
+$l['prostats_newest_members'] = "New members";
+
+$l['prostats_newest_threads'] = "Last posts";
+$l['prostats_topic'] = "Topic";
+$l['prostats_author'] = "Author";
+$l['prostats_replies'] = "Replies";
+
+$l['prostats_most_replies'] = "Most replies";
+
+$l['prostats_most_views'] = "Most views";
+$l['prostats_updated_threads'] = "Last updates";
+$l['prostats_views'] = "Views";
+
+$l['prostats_newest_posts'] = "New posts";
+$l['prostats_subject'] = "Title";
+
+$l['prostats_err_thxplugin'] = "Please active Thank You plugin.";
+$l['prostats_err_undefind'] = "Undefind";
+?>
---
+++ inc/languages/persian/prostats.lang.php
@@ -0,0 +1,31 @@
+<?php
+$l['prostats_prostats'] = "نمایش آمار انجمن";
+
+$l['prostats_username'] = "نام کاربری";
+$l['prostats_posts'] = "ارسال";
+$l['prostats_datetime'] = "تاریخ, زمان";
+$l['prostats_forum'] = "انجمن";
+
+$l['prostats_top_posters'] = "فعال ترین کاربران";
+$l['prostats_top_topreferrers'] = "برترین معرف ها";
+$l['prostats_most_thanks'] = "بیشترین تشکر شده ها";
+$l['prostats_newest_members'] = "کاربران جدید";
+$l['prostats_top_downloads'] = "بیشترین دانلود ها";
+$l['prostats_newest_threads'] = "آخرین ارسالی ها";
+$l['prostats_topic'] = "موضوع";
+$l['prostats_author'] = "آغاز کننده";
+$l['prostats_last_sender'] = "آخرین ارسال";
+$l['prostats_replies'] = "پاسخها";
+
+$l['prostats_most_replies'] = "پر پاسخ ترین تاپیک ها";
+
+$l['prostats_most_views'] = "پر بازدید ترین ارسال ها";
+$l['prostats_updated_threads'] = "آخرین به روز رسانی ها";
+$l['prostats_views'] = "بازدید";
+
+$l['prostats_newest_posts'] = "جدید ترین ارسال ها";
+$l['prostats_subject'] = "موضوع";
+
+$l['prostats_err_thxplugin'] = "لطفاً پلاگین تشکر را فعال نمایید";
+$l['prostats_err_undefind'] = "نامشخص";
+?>
